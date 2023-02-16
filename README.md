90ff30a2d906502ea6c57dccaf273ff51a26fc49
Batuhan Kavlak <batuhan.kavlak@up42.com>
2023-02-16 09:10:42 +0100

    added new details

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# stac-download-asset
+# Scene Download
+
+- Install dependencies
+- Provide a scene id (catid) to the script to download
+- The script will create a folder and download the tiles in it
+
+# Merge
+
+- Go to the scene directory
+- Build VRT
+
+```
+gdalbuildvrt out.vrt *.tif
+```
+
+- Merge & Compress
+
+```
+gdal_translate out.vrt out.tif -a_nodata 0.0 -of GTiff -co BIGTIFF=YES -co TILED=YES -co COMPRESS=JPEG -co PHOTOMETRIC=YCBCR
+```
--- download.py
+++ download.py
@@ -1,25 +1,30 @@
 import requests
 from pathlib import Path
 
-scene_id = "10300500D9F8CC00"
-list_file = "aws_list.txt" # put it to the parent directory
+import boto3
+
+scene_id = "10300100E18CB600"
+
+s3 = boto3.resource(
+        "s3",
+        aws_access_key_id="AKIA47MNXXGLC7N6FZF4",
+        aws_secret_access_key="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",
+    )
+bucket = s3.Bucket(name="maxar-opendata")
+blobs = bucket.objects.filter(Prefix="")
+
+print("Detecting tiles")
+resource_list = [
+    f"https://maxar-opendata.s3.amazonaws.com/{blob.key}" for blob in blobs if scene_id in blob.key
+]
+tile_list = [url for url in resource_list if "visual" in url]
 
 Path(f"SCENES/{scene_id}").mkdir(parents=True, exist_ok=True)
 download_path = Path(f"SCENES/{scene_id}")  
 
-# Convert list
-with open(list_file, "r") as aws_text:
-    lines = aws_text.readlines()
-    tile_list = []
-    for l in lines:
-        as_list = l.split(", ")
-        tile_list.append(as_list[0])
-
-    tile_list = [tile for tile in tile_list if scene_id in tile]
-    clean_list = [tile.strip() for tile in tile_list]
-
 # Download tiles
-for index, tile_url in enumerate(clean_list):
+for index, tile_url in enumerate(tile_list):
+    print(f"downloading {tile_url}")
     r = requests.get(tile_url, allow_redirects=True)
-    with open(download_path / f'tile_{index}.tiff', 'wb') as f:
+    with open(download_path / f'tile_{index}.tif', 'wb') as f:
         f.write(r.content)
